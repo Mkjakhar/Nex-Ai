9b8f86fc28362b3c8419c69978d7aaacdc8839c4
Mohit <mkjakhar909@gmail.com>
2023-06-05 20:46:30 +0530

    add

---
+++ src/components/Algorithum.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const Algorithum = () => {
+  return (
+    <>
+      <section>
+        <Container>
+            
+        </Container>
+      </section>
+    </>
+  );
+};
+
+export default Algorithum;
